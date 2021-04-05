19273b6c49bc8df2d1ffc564ad3d6a3b7c1b9024
Ararat <ararat.martirossyan@jumbo.com>
2021-04-05 19:15:23 +0200

    Re-init

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,18 @@ import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from "./router";
 
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+})
+
 const app = createApp(App)
 
 app.use(urql, { url: 'https://graph.myshelf.info/graphql' })
